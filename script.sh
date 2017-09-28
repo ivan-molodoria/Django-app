0157d36b3d68856592dc1e34d436cb2def4bbb99
knoking7 <imolodoria@gmail.com>
2017-09-28 14:42:18 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+echo $TRAVIS_BRANCH 
 if $TRAVIS_BRANCH == 'master' 
 then  
   sed -i '29,30 s/^/#/' .travis.yml
 else
-  cd spec/dummy
-  sed -i '31,33 s/^/#/' .travis.yml
+    sed -i '31,33 s/^/#/' .travis.yml
 fi
