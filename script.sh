a04466d5cb1afa07309e23798aeffba6a6548fac
knoking7 <imolodoria@gmail.com>
2017-09-28 14:34:34 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,7 @@
-#!/bin/bash 
-docker --version  # document the version travis is using
- pip install awscli # install aws cli w/o sudo
- export PATH=$PATH:/root/.local/bin # put aws in the path
- eval $(aws ecr get-login --region us-west-2 --no-include-email) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
- docker build -t test-repo .
- docker tag test-repo:latest 425987977703.dkr.ecr.us-west-2.amazonaws.com/test-repo:latest
- docker push 425987977703.dkr.ecr.us-west-2.amazonaws.com/test-repo:latest
-
+#!/bin/bash
+if [[ $TRAVIS_BRANCH == 'master' ]]
+  sed -i '29,30 s/^/#/' .travis.yml
+else
+  cd spec/dummy
+  sed -i '31,33 s/^/#/' .travis.yml
+fi
